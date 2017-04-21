6fb190695af8b5bf433b49aed8a646d1cf7cebe6
correlation-one-datathon-team-7 <correlationone7@gmail.com>
2017-04-21 14:35:40 -0400

    attempt to forecast timeline series

---
+++ forecast_model.R
@@ -0,0 +1,15 @@
+library(fma)
+library(fpp)
+
+library(anova)
+library(CRAN)
+library(MASS)
+library(cvTools)
+library(kernlab)
+library(C50)
+library(stats)
+library(e1071)
+library(gtools)
+
+ecoli_d <- read.csv("datasets/Ecoli_data.csv", header = F, sep = ',')
+
